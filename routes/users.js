990065bda53f1748df135e47ba55f02386460829
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-26 20:50:25 +0000

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+
   router.get("/", (req, res) => {
     knex
       .select("*")
@@ -12,6 +13,152 @@ module.exports = (knex) => {
       .then((results) => {
         res.json(results);
     });
+  });
+
+
+// login, NEEDS
+//  Users
+//    name
+//    email
+//    password
+  router.get("/login", (req, res) => {
+
+    knex
+      .select("*")
+      .from("users")
+      .then((results) => {
+        res.json(results);
+    });
+
+  });
+
+  router.post("/register", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/login", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/logout", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+
+// homepage, NEEDS
+//   Resources:
+//     Title
+//     link
+//     descritpion
+//   Likes
+//   Ratings
+//     rating
+
+  router.get("/users/:user_id/resources", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+// new link, NEEDS
+  router.get("/users/:user_id/resources/new", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/resources", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+// needs single RESOURCE with the current id/name, LIKES, COMMENTS, RATING
+  router.get("/resources/:res_id/show", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.get("/users/:user_id/edit", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/users/", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/res/:id/like", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.post("/res/:id/comment", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.put("/res/:id/rate", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
+  });
+
+  router.delete("/res/:id/like", (req, res) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
   });
 
   return router;
