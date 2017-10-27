954733c42c53e978cdc658b3dabded7d14f9f6da
Thomas Nicholson <nicholmen@gmail.com>
2017-10-27 03:24:13 +0000

    first commit. big commit. created html skeleton for my resources, created partials, created some scss and 2 jquery functions in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,16 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+  $( ".card-block" ).click(function() {
+    $( "#my_outsights" ).hide( 0, function() {
+      $("#expanded_resource").show( 0, function() {
+      })
+    });
+  });
+  $( "#resources_toggle" ).click(function() {
+    $( "#expanded_resource" ).hide( 0, function() {
+      $("#my_outsights").show( 0, function() {
+      })
+    });
+  })
 });
